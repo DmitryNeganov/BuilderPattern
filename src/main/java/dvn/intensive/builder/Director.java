d9bfb05a49d23aed2b472ba40f4c17b3b003d0f8
dvn <glorynewfound@yandex.ru>
2023-01-30 12:47:25 +0300

    Initial commit

---
+++ src/main/java/dvn/intensive/builder/Director.java
@@ -0,0 +1,13 @@
+package dvn.intensive.builder;
+
+public class Director {
+    public Lunch getLunch(LunchBuilder lunchBuilder) {
+        Lunch lunch = lunchBuilder.reset();
+        lunchBuilder.addSoup()
+                .addSalad()
+                .addMainDish()
+                .addBeverage()
+                .addDesert();
+        return lunch;
+    }
+}
---
+++ src/main/java/dvn/intensive/controller/HomeController.java
@@ -0,0 +1,43 @@
+package dvn.intensive.controller;
+
+import dvn.intensive.builder.Director;
+import dvn.intensive.builder.Lunch;
+import dvn.intensive.builder.concrete_builders.FullLunchBuilder;
+import dvn.intensive.builder.concrete_builders.LightLunchBuilder;
+import dvn.intensive.builder.concrete_builders.StandardLunchBuilder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String showHomeView() {
+        return "home";
+    }
+
+    @RequestMapping("/lightLunch")
+    public String getLightLunch(Model model) {
+        Lunch lunch = (new Director()).getLunch(new LightLunchBuilder());
+        String lightLunch = lunch.toString();
+        model.addAttribute("lunch", lightLunch);
+        return "lunch";
+    }
+
+    @RequestMapping("/standardLunch")
+    public String getStandardLunch(Model model) {
+        Lunch lunch = (new Director()).getLunch(new StandardLunchBuilder());
+        String lightLunch = lunch.toString();
+        model.addAttribute("lunch", lightLunch);
+        return "lunch";
+    }
+
+    @RequestMapping("/fullLunch")
+    public String getFullLunch(Model model) {
+        Lunch lunch = (new Director()).getLunch(new FullLunchBuilder());
+        String lightLunch = lunch.toString();
+        model.addAttribute("lunch", lightLunch);
+        return "lunch";
+    }
+}
